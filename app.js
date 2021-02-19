a7170bd87894e2a05e0b8e01873977024f717ce1
youme <mrsimplelife@naver.com>
2021-02-19 16:32:30 +0900

    add local login

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ sequelize
     console.error(err);
   });
 const pageRouter = require("./routes/page");
+const authRouter = require("./routes/auth");
+
 const app = express();
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "html");
@@ -38,6 +40,7 @@ app.use(
 );
 
 app.use("/", pageRouter);
+app.use("/auth", authRouter);
 app.use((req, res, next) => {
   const err = new Error(`${req.method} ${req.url}라우터가 없습니다.`);
   err.status = 404;

8dcf07254bcb0f2793f0005091328a5e904d0e6f
youme <mrsimplelife@naver.com>
2021-02-19 20:44:53 +0900

    add search hashtag

--- routes/page.js
+++ routes/page.js
@@ -1,10 +1,9 @@
 const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
-const { Post, User } = require("../models");
+const { Post, User, Hashtag } = require("../models");
 const router = require("express").Router();
 
 router.use((req, res, next) => {
   res.locals.user = req.user;
-  console.log(req.user);
   res.locals.followerCount = req.user ? req.user.Followers.length : 0;
   res.locals.followingCount = req.user ? req.user.Followings.length : 0;
   res.locals.followingIdList = req.user
@@ -31,6 +30,28 @@ router.get("/", async (req, res, next) => {
   } catch (error) {
     console.log(error);
     next(error);
+  }
+});
+router.get("/hashtag", async (req, res, next) => {
+  const query = decodeURIComponent(req.query.hashtag);
+  if (!query) {
+    return res.redirect("/");
+  }
+  try {
+    let posts = [];
+    const hashtag = await Hashtag.findOne({ where: { title: query } });
+    if (hashtag) {
+      posts = await hashtag.getPosts({
+        include: [{ model: User, attributes: ["id", "nick"] }],
+      });
+    }
+    return res.render("main", {
+      title: `${query} | nodejack`,
+      twits: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
   }
 });
 module.exports = router;

7dab77f4f3bdeb5dae29cec1df3e327fa641a0ac
youme <mrsimplelife@naver.com>
2021-02-20 16:10:44 +0900

    Test middlewares

--- routes/middlewares.test.js
+++ routes/middlewares.test.js
@@ -1,3 +1,50 @@
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
 test("1+1=2", () => {
   expect(1 + 1).toEqual(2);
+});
+describe("isLoggedIn", () => {
+  const res = {
+    status: jest.fn(() => res),
+    send: jest.fn(),
+  };
+  const next = jest.fn();
+
+  test("should call next", () => {
+    const req = {
+      isAuthenticated: jest.fn(() => true),
+    };
+    isLoggedIn(req, res, next);
+    expect(next).toBeCalledTimes(1);
+  });
+  test("should response error code", () => {
+    const req = {
+      isAuthenticated: jest.fn(() => false),
+    };
+    isLoggedIn(req, res, next);
+    expect(res.status).toBeCalledWith(403);
+    expect(res.send).toBeCalledWith("no loggedIn");
+  });
+});
+describe("isNotLoggedIn", () => {
+  const res = {
+    status: jest.fn(() => res),
+    send: jest.fn(),
+    redirect: jest.fn(),
+  };
+  const next = jest.fn();
+  test("should call next", () => {
+    const req = {
+      isAuthenticated: jest.fn(() => false),
+    };
+    isNotLoggedIn(req, res, next);
+    expect(next).toBeCalledTimes(1);
+  });
+  test("should redirect", () => {
+    const req = {
+      isAuthenticated: jest.fn(() => true),
+    };
+    isNotLoggedIn(req, res, next);
+    const message = encodeURIComponent("you aleady logged in");
+    expect(res.redirect).toBeCalledWith(`/?error=${message}`);
+  });
 });
